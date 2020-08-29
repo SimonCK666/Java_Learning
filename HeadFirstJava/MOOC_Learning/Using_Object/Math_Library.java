8afad3c59957589962ea63b693006f8e01679aa6
SimonCK666 <SimonYang223@163.com>
2020-08-29 10:14:11 +0800

    Java_Learning

---
+++ HeadFirstJava/MOOC_Learning/Using_Object/Math_Library.java
@@ -0,0 +1,11 @@
+public class Math_Library {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-12));
+        // 0-1之间的随机数
+        System.out.println(Math.random());
+        // 把小数部分四舍五入
+        System.out.println(Math.round(10.234));
+        // 2的3次方
+        System.out.println(Math.pow(2, 3));
+    }
+}
---
+++ HeadFirstJava/MOOC_Learning/Using_Object/stringType.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Scanner;
+public class stringType {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = new String("Hello");
+        System.out.println(s + " world~");
+
+        String x;
+        x = in.nextLine();
+        System.out.println(x);
+
+        String s1 = "abc";
+        String s2 = "abcd";
+        System.out.println(s1.compareTo(s2));
+    }
+}
